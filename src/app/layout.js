acb7517249e3c3e9e8a79d841ca1d80ebeadcb6b
byGAR1K <santasweezy@gmail.com>
2025-09-12 16:56:20 +0300

    fix layout undefined Header Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
+'use client';
+
 export default function RootLayout({ children }) {
-    return (
-      <html>
-        <body className="bg-[#1E293B] text-white font-sans flex flex-col min-h-screen">
-          <Header /> {/* твоя шапка с лого и навигацией */}
-          <main className="flex-grow max-w-5xl mx-auto py-12 px-6">
-            {children}
-          </main>
-          <Footer /> {/* футер */}
-        </body>
-      </html>
-    );
-  }
+  return (
+    <html lang="ru">
+      <body className="bg-[#1E293B] text-white font-sans">
+        {children}
+      </body>
+    </html>
+  );
+}
