084f456d7ad75d8771012d103a5063a8b0beafdf
byGAR1K <santasweezy@gmail.com>
2025-09-12 18:40:10 +0300

    Обновил код Telegram Wallet кнопки и вкладку Telegram (X2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ function TelegramButton() {
     const script = document.createElement('script');
     script.src = "https://telegram.org/js/telegram-widget.js?7";
     script.async = true;
-    script.setAttribute("data-telegram-login", "@SantaExchange_bot"); // ваш бот
+    script.setAttribute("data-telegram-login", "SantaExchange_bot"); // ваш бот
     script.setAttribute("data-size", "large");
     script.setAttribute("data-userpic", "true");
     script.setAttribute("data-request-access", "write");
-    script.setAttribute("data-auth-url", "https://swaply-txjc.vercel.app//api/telegram-auth");
+    script.setAttribute("data-auth-url", "https://swaply-txjc.vercel.app/api/telegram-auth");
 
     const container = document.getElementById('telegram-button');
     if (container) container.appendChild(script);
